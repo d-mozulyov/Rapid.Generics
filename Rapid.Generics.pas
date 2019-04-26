706cab7e96e12be9d6a9c3b57be44a3047b6b16f
d.mozulev <d.mozulev@osnovalab.ru>
2019-04-26 16:12:48 +0300

    License year fixed

--- Rapid.Generics.pas
+++ Rapid.Generics.pas
@@ -1,7 +1,7 @@
 unit Rapid.Generics;
 
 {******************************************************************************}
-{ Copyright (c) 2019 Dmitry Mozulyov                                           }
+{ Copyright (c) 2017-2019 Dmitry Mozulyov                                           }
 {                                                                              }
 { Permission is hereby granted, free of charge, to any person obtaining a copy }
 { of this software and associated documentation files (the "Software"), to deal}
